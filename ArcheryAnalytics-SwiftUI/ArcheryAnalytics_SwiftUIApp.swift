7d1702a95e830652e12ef6e748b79721d8898024
Alex de Vera <adever2@gmail.com>
2023-11-26 19:40:10 -0600

    Initial Commit

---
+++ ArcheryAnalytics-SwiftUI/ArcheryAnalytics_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcheryAnalytics_SwiftUIApp.swift
+//  ArcheryAnalytics-SwiftUI
+//
+//  Created by Alex de Vera on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ArcheryAnalytics_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
